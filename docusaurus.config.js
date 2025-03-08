89cba9036d3c37b3a144a2b1b1d3b153c8f9635d
Kevin MacPhail <kmacphail@pipelinefx.com>
2025-03-08 09:37:36 -0400

    Update baseURL
    Changed to docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://pipelinefx.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs_test_2/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
